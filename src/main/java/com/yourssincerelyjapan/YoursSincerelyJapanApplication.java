14c09671b3ad5bd10cb79060b68e0e7249076c3d
Nikola <nikola.malinov.krastev@gmail.com>
2023-10-03 13:51:33 +0300

    Initial commit

---
+++ src/main/java/com/yourssincerelyjapan/YoursSincerelyJapanApplication.java
@@ -0,0 +1,13 @@
+package com.yourssincerelyjapan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoursSincerelyJapanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoursSincerelyJapanApplication.class, args);
+	}
+
+}
