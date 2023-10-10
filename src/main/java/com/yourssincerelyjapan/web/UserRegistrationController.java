a161b1176497c3eb1ff0b9219b3bbaab95ea540f
Nikola <nikola.malinov.krastev@gmail.com>
2023-10-10 15:59:43 +0300

    Added Registration page; Client-side validation; Few style changes

---
+++ src/main/java/com/yourssincerelyjapan/web/UserRegistrationController.java
@@ -0,0 +1,19 @@
+package com.yourssincerelyjapan.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/users")
+public class UserRegistrationController {
+
+    @GetMapping("/registration")
+    public ModelAndView userRegistration(ModelAndView modelAndView) {
+
+        modelAndView.setViewName("registration");
+
+        return modelAndView;
+    }
+}
