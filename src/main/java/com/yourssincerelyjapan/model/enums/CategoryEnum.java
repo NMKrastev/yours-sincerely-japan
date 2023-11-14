f84127e1fb68a7920bce689331622d0a8e7a3ad4
Nikola <nikola.malinov.krastev@gmail.com>
2023-11-14 12:22:06 +0200

    Added couple more categories; Style changes - hiding pagination when no content is available

--- src/main/java/com/yourssincerelyjapan/model/enums/CategoryEnum.java
+++ src/main/java/com/yourssincerelyjapan/model/enums/CategoryEnum.java
@@ -6,5 +6,7 @@ public enum CategoryEnum {
     CARS,
     TIPS,
     GUIDES,
-    FOOD
+    FOOD,
+    LIFE,
+    WORK
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,9 +2,11 @@ INSERT INTO roles (name)
 VALUES ('ADMIN'),
        ('USER');
 
-INSERT INTO categories (name, latest_created_article)
+INSERT INTO categories (`name`, `latest_created_article`)
 VALUES ('TRAVEL', NULL),
        ('CARS', NULL),
        ('TIPS', NULL),
        ('GUIDES', NULL),
-       ('FOOD', NULL);
+       ('FOOD', NULL),
+       ('LIFE', NULL),
+       ('WORK', NULL);
